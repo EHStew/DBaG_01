6ae50f62c83afc47e54b62b2bfc27a1fe5710a10
Elias <103904751+EHStew@users.noreply.github.com>
2023-08-19 14:22:07 -0500

    add player movement. add blink powerupp icon

--- Sprite2D.gd
+++ Sprite2D.gd
@@ -1,1 +1,30 @@
 extends Sprite2D
+@export var speed = 400
+var screen_size
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func _process(delta): # Player Movement
+	# Start player with 0 velocity, 0.0 vector
+	var velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	# Reign in velocity so you dont move 2Xspeed diagonally
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	
+	## Using delta ensures that our movement speed in-game will not be impacted by framerate
+	position += velocity * delta
+	
+	# Restrict player movement to view boundraries
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
