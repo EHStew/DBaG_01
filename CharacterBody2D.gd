874a44b94d89a4c9de80e66548407d56ad36fa4d
Elias <103904751+EHStew@users.noreply.github.com>
2023-08-21 21:37:03 -0500

    finish player blink dash

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,21 +1,28 @@
 extends CharacterBody2D
 
+var dir = Vector2.ZERO
+var dashDir = Vector2.ZERO
+
 var screenSize
 
 
 const maxSpeed = 800
 const accel = 10000
 const friction = 8000
+const dashMod = 10
 
 var canMove = true
 var walkSpeed = accel
-var dashSpeed = accel * 10
+
+
+var dashSpeed = accel * dashMod
+var dashSpeedCap = maxSpeed * 6
 var canDash = true
 var dashing = false
-var dashFriction
+var dashFriction = friction 
+var dashLength = .2
 
-
-var dir = Vector2.ZERO
+var blinkCounter = 1
 
 func _ready():
 	screenSize = get_viewport_rect().size
@@ -30,16 +37,23 @@ func _physics_process(delta):
 
 
 func get_dir():
-	dir.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed(("move_left")))
-	# 1 = right | -1 = left
-	dir.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed(("move_up")))
-	return dir.normalized()
+	if not dashing:
+		dir.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed(("move_left")))
+		# 1 = right | -1 = left
+		dir.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed(("move_up")))
+		if dir != Vector2.ZERO:
+			dashDir.x = dir.x
+			dashDir.y = dir.y
+		return dir.normalized()
+	elif dashing:
+		return dashDir.normalized()
 
 
 
 
 func player_movement(delta):
 	dir = get_dir()
+	dash()
 	if not dashing:
 		if dir == Vector2.ZERO:
 			if velocity.length() > (friction * delta):
@@ -47,24 +61,34 @@ func player_movement(delta):
 			else:
 				velocity = Vector2.ZERO
 		else:
-			velocity += (dir * walkSpeed * delta) 
-			velocity = velocity.limit_length(maxSpeed)
+				velocity += (dir * walkSpeed * delta)
+				velocity = velocity.limit_length(maxSpeed) 
 	else:
-		velocity += (dir * dashSpeed * delta) 
-		
+		velocity += (dashDir * dashSpeed * delta)
+		velocity = velocity.limit_length(dashSpeedCap) 
+
+
 	move_and_slide()
 	# Restrict player movement to view boundraries
 	position.x = clamp(position.x, 0, screenSize.x)
 	position.y = clamp(position.y, 0, screenSize.y)
 
+
+
 func dash():
-	if Input.is_action_pressed("dash"):
-		print("Dashing")
-		dashing = true
-		walkSpeed = dashSpeed
-		await  get_tree().create_timer(1.0).timeout
-		walkSpeed = accel
-		dashing = false
+	if Input.is_action_just_pressed("dash"):
+		if blinkCounter >= 1:
+			print("Dashing")
+			dashing = true
+			await  get_tree().create_timer(.05).timeout
+			self.hide()
+			await  get_tree().create_timer(.08).timeout
+			dashSpeedCap = 1000
+			await  get_tree().create_timer(dashLength/3).timeout
+			dashing = false
+			await  get_tree().create_timer(.02).timeout
+			self.show()
+			dashSpeedCap = maxSpeed * 6
 
 
 
