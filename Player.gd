b5058e405414887d668b8ae85d069c7bbef876db
Elias <103904751+EHStew@users.noreply.github.com>
2023-08-22 06:39:51 -0500

    add bullet time and slow time pickup

--- Player.gd
+++ Player.gd
@@ -1,44 +1,116 @@
-extends Area2D
-@export var speed = 400
-var screen_size
-var blink_counter = 0
+extends CharacterBody2D
+
+var dir = Vector2.ZERO
+var dashDir = Vector2.ZERO
+
+var screenSize
+
+
+const maxSpeed = 800
+const accel = 10000
+const friction = 8000
+const dashMod = 10
+
 var canMove = true
+var walkSpeed = accel
+
+var timeSlowed = false
+var slowFactor = 1
+
+
+var dashSpeed = accel * dashMod
+var dashSpeedCap = maxSpeed * 6
 var canDash = true
-var dashSpeed = 50
-var dashFrames = 0
+var dashing = false
+var dashFriction = friction 
+var dashLength = .2
+
+var blinkCounter = 0
+var timeCounter = 0
+
+func _ready():
+	screenSize = get_viewport_rect().size
 
 
 
-func _process(delta): # Player Movement
-	# Start player with 0 velocity, 0.0 vector
-	var velocity = Vector2.ZERO
-	if canMove:
-		if Input.is_action_pressed("move_up"):
-			velocity.y -= 1
-		if Input.is_action_pressed("move_down"):
-			velocity.y += 1
-		if Input.is_action_pressed("move_left"):
-			velocity.x -= 1
-		if Input.is_action_pressed("move_right"):
-			velocity.x += 1
-		# Reign in velocity so you dont move 2Xspeed diagonally
-		if velocity.length() > 0:
-			velocity = velocity.normalized() * speed
-			
-	if Input.is_action_pressed("dash"):
-		if canDash:
-			
-			canDash = false
-			canMove = false
-			if velocity.x > 0:
-					velocity.x = velocity.x*dashSpeed
-			elif velocity.x < 0:
-					velocity.x = velocity.x*dashSpeed
-			if velocity.y > 0:
-					velocity.y = velocity.y*dashSpeed
-			elif velocity.x < 0:
-					velocity.y = velocity.y*dashSpeed
-		
-	## Using delta ensures that our movement speed in-game will not be impacted by framerate
-	position += velocity * delta
-	
+
+func _physics_process(delta):
+	player_movement(delta)
+
+
+
+
+func get_dir():
+	if not dashing:
+		dir.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed(("move_left")))
+		# 1 = right | -1 = left
+		dir.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed(("move_up")))
+		if dir != Vector2.ZERO:
+			dashDir.x = dir.x
+			dashDir.y = dir.y
+		return dir.normalized()
+	elif dashing:
+		return dashDir.normalized()
+
+
+
+
+func player_movement(delta):
+	dir = get_dir()
+	dash()
+	slow_time()
+	if not dashing:
+		if dir == Vector2.ZERO:
+			if velocity.length() > (friction * delta):
+				velocity -= velocity.normalized() * (friction * delta)
+			else:
+				velocity = Vector2.ZERO
+		else:
+				velocity += (dir * walkSpeed * delta)
+				velocity = velocity.limit_length(maxSpeed) 
+	else:
+		velocity += (dashDir * dashSpeed * delta)
+		velocity = velocity.limit_length(dashSpeedCap) 
+
+
+	move_and_slide()
+	# Restrict player movement to view boundraries
+	position.x = clamp(position.x, 0, screenSize.x)
+	position.y = clamp(position.y, 0, screenSize.y)
+
+
+
+func dash():
+	if Input.is_action_just_pressed("dash"):
+		if blinkCounter >= 1:
+			print("Dashing")
+			dashing = true
+			blinkCounter -= 1
+			await  get_tree().create_timer(.05).timeout
+			self.hide()
+			await  get_tree().create_timer(.08).timeout
+			dashSpeedCap = 1000
+			await  get_tree().create_timer(dashLength/3).timeout
+			dashing = false
+			await  get_tree().create_timer(.02).timeout
+			self.show()
+			dashSpeedCap = maxSpeed * 6
+
+
+
+func slow_time():
+	if Input.is_action_just_pressed("use_power"):
+		if timeCounter >= 1:
+			timeCounter -= 1
+			timeSlowed = true
+			print("slowing time")
+			slowFactor = 0.5
+			await  get_tree().create_timer(2).timeout
+			timeSlowed = false
+			slowFactor = 1
+	else:
+		pass
+
+
+func game_over():
+	get_tree().quit()
--- enemy.gd
+++ enemy.gd
@@ -1,30 +1,28 @@
 extends Area2D
 
-@export var move_speed : float = 30
+@export var move_speed : float = 300
 @export var move_dir : Vector2
 
 var start_pos : Vector2
 var target_pos : Vector2
 
+@onready var target = $"/root/Main/Player"
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	start_pos = global_position
-	target_pos = start_pos + move_dir
-
-
+	pass
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	global_position = global_position.move_toward(target_pos, move_speed * delta)
-	
-	if global_position == target_pos:
-		if global_position == start_pos:
-			target_pos = start_pos + move_dir
-		else:
-			target_pos = start_pos
+		target_pos = target.global_position
+		if global_position != target_pos:
+			global_position = global_position.move_toward(target_pos, move_speed * (delta * target.slowFactor))
 
 
 
 
-func _on_area_entered(area):
-	if area.is_in_group("Player"):
-		area.game_over()
+func _on_body_entered(body):
+	if target.dashing == false:
+		if body.is_in_group("Player"):
+			body.game_over()
