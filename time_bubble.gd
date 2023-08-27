571791607fa19310f00fa5ebed3ffd76b9dc3068
Elias <103904751+EHStew@users.noreply.github.com>
2023-08-27 00:47:01 -0500

    add self drawing timer circle to time_bubble

--- time_bubble.gd
+++ time_bubble.gd
@@ -1,10 +1,9 @@
-@tool
 extends Node2D
 
 var rotation_angle = 50
 var angle_from = 0
-var angle_to = 180
-var circleSpeed = 10
+var angle_to = 0
+var timerWidth = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,19 +12,22 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	angle_from += rotation_angle * delta
-	angle_to += rotation_angle * delta
+	# Rotate arc 
+	# angle_from += rotation_angle * delta
+	# angle_to += rotation_angle * delta
 
 	# We only wrap angles when both of them are bigger than 360.
 	if angle_from > 360 and angle_to > 360:
 		angle_from = wrapf(angle_from, 0, 360)
 		angle_to = wrapf(angle_to, 0, 360)
+	
+	if angle_to < 360:
+		angle_to += 30
 	queue_redraw()
-
 	
 	
 func draw_circle_arc(center, radius, angle_from, angle_to, color):
-	var nb_points = 32
+	var nb_points = 64
 	var points_arc = PackedVector2Array()
 
 	for i in range(nb_points + 1):
@@ -33,12 +35,26 @@ func draw_circle_arc(center, radius, angle_from, angle_to, color):
 		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
 
 	for index_point in range(nb_points):
-		draw_line(points_arc[index_point], points_arc[index_point + 1], color)
+		draw_line(points_arc[index_point], points_arc[index_point + 1], color, timerWidth, true)
+	
+	
+	
+func draw_timer_arc(center, radius, angle_from, angle_to, color):
+	var nb_points = 64
+	var points_arc = PackedVector2Array()
+
+	for i in range(nb_points + 1):
+		var angle_point = deg_to_rad(angle_from + i * (angle_to-angle_from) / nb_points - 90)
+		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
+
+	for index_point in range(nb_points):
+		draw_line(points_arc[index_point], points_arc[index_point + 1], color, timerWidth * .80, true)
 
 
 func _draw():
 	var center = Vector2.ZERO
-	var radius = 80
-	var color = Color(1.0, 0.0, 0.0)
+	var radius = 150
+	var color = Color(1.0, 1.0, 1.0)
 
 	draw_circle_arc( center, radius, angle_from, angle_to, color )
+	draw_timer_arc(center, radius, angle_from, angle_to, Color(0.0, 0.0, 0.0))
